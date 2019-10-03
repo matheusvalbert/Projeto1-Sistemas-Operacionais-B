f23d44bb40a10442d838a9fd2b2c121f7b1af8cb
gabrielala10 <gabriela_m18@outlook.com>
2019-10-03 17:39:42 -0300

    Modulo criptográfico - Erros

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
-obj-m+=teste.o
-
+obj-m+=cripto-demo.o
+ 
 all:
 	make -C /lib/modules/$(shell uname -r)/build/ M=$(PWD) modules
-
 clean:
 	make -C /lib/modules/$(shell uname -r)/build/ M=$(PWD) clean
-
---
+++ cripto-demo.c
@@ -0,0 +1,154 @@
+
+
+/* 
+ * Simple demo explaining usage of the Linux kernel CryptoAPI.
+ * By Michal Ludvig <michal@logix.cz>
+ *    http://www.logix.cz/michal/
+ */
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/crypto.h>
+#include <linux/mm.h>
+#include <linux/scatterlist.h>
+#include <crypto/skcipher.h>
+
+#define PFX "cryptoapi-demo: "
+
+MODULE_AUTHOR("Michal Ludvig <michal@logix.cz>");
+MODULE_DESCRIPTION("Simple CryptoAPI demo");
+MODULE_LICENSE("GPL");
+
+/* ====== CryptoAPI ====== */
+
+#define DATA_SIZE       16
+
+static void
+hexdump(unsigned char *buf, unsigned int len)
+{
+        while (len--)
+	{
+	        printk("%02x", *buf++);
+	}
+
+        printk("\n");
+}
+
+static void
+cryptoapi_demo(void)
+{
+        /* config options */
+        char key[16]={"0123456789ABCDEF"}, iv[16]={"0123456789ABCDEF"};
+
+        /* local variables */
+	struct skcipher_def sk;
+        struct crypto_skcipher *tfm = NULL;
+	struct scatterlist sg[8];
+	struct skcipher_request *req;
+        int ret;
+        char *input, *encrypted, *decrypted;
+
+        memset(key, 0, sizeof(key));
+        memset(iv, 0, sizeof(iv));
+	//iv="0123456789ABCDEF";
+	//key="0123456789ABCDEF";
+        tfm = crypto_alloc_skcipher ("cbc(aes)", 0, 0);
+
+        if (IS_ERR(tfm)) {
+                printk("could nor allocate skcipher tfm\n");
+                return;
+        }
+
+
+	req = skcipher_request_alloc(tfm, GFP_KERNEL);
+    	if (!req) {
+        	printk("could not allocate tfm request\n");
+        	goto out;
+    	}
+
+        ret = crypto_skcipher_setkey(tfm, key, sizeof(key));
+
+        if (ret) {
+                printk(KERN_ERR PFX "error ret\n");
+                goto out;
+        }
+
+        input = kmalloc(DATA_SIZE, GFP_KERNEL);
+        if (!input) {
+                printk(KERN_ERR PFX "kmalloc(input) failed\n");
+                goto out;
+        }
+
+        encrypted = kmalloc(DATA_SIZE, GFP_KERNEL);
+        if (!encrypted) {
+                printk(KERN_ERR PFX "kmalloc(encrypted) failed\n");
+                kfree(input);
+                goto out;
+        }
+
+        decrypted = kmalloc(DATA_SIZE, GFP_KERNEL);
+        if (!decrypted) {
+                printk(KERN_ERR PFX "kmalloc(decrypted) failed\n");
+                kfree(encrypted);
+                kfree(input);
+                goto out;
+        }
+	
+        memset(input,0, DATA_SIZE);
+	input[0] = 0x11;
+	input[1] = 0x22;
+
+	sk.tfm = tfm;
+	sk.req = req;
+	sg_init_one(&sk.sg, input, DATA_SIZE);
+	//sg_init_one(&sg[1], encrypted, DATA_SIZE);
+	//sg_init_one(&sg[2], decrypted, DATA_SIZE);
+	skcipher_request_set_crypt(req, &sk.sg, &sk.sg, 16, ivdata);
+    init_completion(&sk.result.completion);
+
+	ret = test_skcipher_encdec(&sk, 1);
+    if (ret)
+        goto out;
+	printk(KERN_ERR PFX "ANTES: "); hexdump(encrypted, 2);//data_size ultimo parametro
+	 printk(KERN_ERR PFX "DPS: "); hexdump(decrypted, 2);
+	skcipher_request_set_crypt(req, &sg[0], &sg[1], DATA_SIZE, iv);
+	crypto_skcipher_encrypt(req);
+	skcipher_request_set_crypt(req, &sg[1], &sg[2], DATA_SIZE, iv);
+	crypto_skcipher_decrypt(req);
+	printk(KERN_ERR PFX "IN: "); hexdump(input, 2);
+        printk(KERN_ERR PFX "ANTES: "); hexdump(encrypted, 2);//data_size ultimo parametro
+	 printk(KERN_ERR PFX "DPS: "); hexdump(decrypted, 2);
+
+        if (memcmp(input, decrypted, DATA_SIZE) != 0)
+                printk(KERN_ERR PFX "FAIL: input buffer != decrypted buffer\n");
+        else
+                printk(KERN_ERR PFX "PASS: encryption/decryption verified\n");
+
+        kfree(decrypted);
+        kfree(encrypted);
+        kfree(input);
+
+out:
+        crypto_free_skcipher(tfm);
+}
+
+/* ====== Module init/exit ====== */
+
+static int __init
+init_cryptoapi_demo(void)
+{
+        cryptoapi_demo();
+
+        return 0;
+}
+
+static void __exit
+exit_cryptoapi_demo(void)
+{
+}
+
+module_init(init_cryptoapi_demo);
+module_exit(exit_cryptoapi_demo);
+
+
