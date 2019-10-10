a4d7f45b65d4c8b21dcdbbf2dbd420f74a310102
Matheus Valbert <matheusvalbert@gmail.com>
2019-10-10 17:37:09 -0300

    Add files via upload

---
+++ hash.c
@@ -0,0 +1,92 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/crypto.h>
+#include <linux/mm.h>
+#include <linux/scatterlist.h>
+#include <crypto/skcipher.h>
+#include <linux/kernel.h>
+#include <linux/stat.h>   
+#include <linux/device.h>       
+#include <linux/fs.h>             
+#include <linux/uaccess.h>
+#include <crypto/hash.h>
+
+MODULE_LICENSE("GPL");
+
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
+};
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg)
+{
+    struct sdesc *sdesc;
+    int size;
+
+    size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+    sdesc = kmalloc(size, GFP_KERNEL);
+    if (!sdesc)
+        return ERR_PTR(-ENOMEM);
+    sdesc->shash.tfm = alg;
+    return sdesc;
+}
+
+static int calc_hash(struct crypto_shash *alg,
+             const unsigned char *data, unsigned int datalen,
+             unsigned char *digest)
+{
+    struct sdesc *sdesc;
+    int ret;
+    u8 out[10];
+    u8 in[10] = {"aaaaaaaaaa"};
+    sdesc = init_sdesc(alg);
+    if (IS_ERR(sdesc)) {
+        pr_info("can't alloc sdesc\n");
+        return PTR_ERR(sdesc);
+    }
+
+    ret = crypto_shash_digest(&sdesc->shash, in, 10, out);
+    kfree(sdesc);
+
+
+    printk("%p", out);
+
+    return ret;
+}
+
+static int test_hash(const unsigned char *data, unsigned int datalen,
+             unsigned char *digest)
+{
+    struct crypto_shash *alg;
+    char *hash_alg_name = "sha1";
+    int ret;
+
+    alg = crypto_alloc_shash(hash_alg_name, 0, 0);
+    if (IS_ERR(alg)) {
+            pr_info("can't alloc alg %s\n", hash_alg_name);
+            return PTR_ERR(alg);
+    }
+    ret = calc_hash(alg, data, datalen, digest);
+    crypto_free_shash(alg);
+    return ret;
+}
+
+static int __init
+init_cryptoapi_demo(void)
+{
+	char data[10] = {"ola"};
+	char digest[100];
+
+	test_hash(data, 10, digest);
+	
+        return 0;
+}
+
+static void __exit
+exit_cryptoapi_demo(void)
+{
+}
+
+module_init(init_cryptoapi_demo);
+module_exit(exit_cryptoapi_demo);
