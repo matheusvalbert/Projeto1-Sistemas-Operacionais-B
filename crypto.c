252c29cb1bc57ed2af13f46a608ff082e43b39cf
tomasmartinolli <42442472+tomasmartinolli@users.noreply.github.com>
2019-10-15 01:01:38 -0300

    Add files via upload

--- crypto.c
+++ crypto.c
@@ -1,53 +1,45 @@
+#include <crypto/hash.h>
+#include <linux/err.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <linux/crypto.h>
-#include <linux/mm.h>
 #include <linux/scatterlist.h>
-#include <crypto/skcipher.h>
-#include <linux/kernel.h>
-#include <linux/stat.h>   
-#include <linux/device.h>       
-#include <linux/fs.h>             
-#include <linux/uaccess.h>   
-#include <linux/mutex.h>    
-#define  DEVICE_NAME "crypto"  
-#define  CLASS_NAME  "cryptoapi"        
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <crypto/rng.h>
+#include <crypto/md5.h>
+#include <crypto/sha.h>
+#include <crypto/internal/skcipher.h>
+#include <linux/jiffies.h>
+#include <linux/mutex.h> 
 
-#define PFX "cryptoapi: "
-
-MODULE_AUTHOR("Grupo SO");
-MODULE_DESCRIPTION("Simple CryptoAPI");
+MODULE_AUTHOR("Projeto1 SO_B");
+MODULE_DESCRIPTION("crypto api");
 MODULE_LICENSE("GPL");
-
-/* ====== CryptoAPI ====== */
+#define  DEVICE_NAME "crypto"
+#define  CLASS_NAME  "cryptoapi"
 
 #define DATA_SIZE       16
 
-static char *key = "";//Para receber parâmetros
-static char *iv = "";
+///////////////////////////////////////////////////////////////////////////
+static DEFINE_MUTEX(crypto_mutex);
 static int    majorNumber;              
-static int     dev_open(struct inode *, struct file *);//Para receber as funções
+static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
-static struct class*  cryptoClass  = NULL; ///< The device-driver class struct pointer
-static struct device* cryptoDevice = NULL; ///< The device-driver device struct pointer
+static struct class*  cryptoClass  = NULL;
+static struct device* cryptoDevice = NULL;
+
+
 static char   msg[256] = {0};
 static int tamanhomsg = 0;
-static DEFINE_MUTEX(crypto_mutex);
 
+static char *key_getu = "";
+static char *iv_get = "";
 
-static	struct crypto_skcipher *tfm = NULL;
-static	struct scatterlist sg[2];
-static	struct skcipher_request *req = NULL;
-static  int ret;
-static  char *input, *output;
-
-module_param(key, charp, 0000);
-MODULE_PARM_DESC(key, "A string");
-module_param(iv, charp, 0000);
-MODULE_PARM_DESC(iv, "A string");
+module_param(key_getu, charp, 0000);
+MODULE_PARM_DESC(key_getu, "A string");
+module_param(iv_get, charp, 0000);
+MODULE_PARM_DESC(iv_get, "A string");
 
 static struct file_operations fops =
 {
@@ -57,92 +49,189 @@ static struct file_operations fops =
    .release = dev_release,
 };
 
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
+};
 
+///////////////////////////////////////////////////////////////////////////
+
+struct tcrypt_result {
+	struct completion completion;
+	int err;
+};
+
+static void test_skcipher_cb(struct crypto_async_request *req, int error)
+{
+	struct tcrypt_result *result = req->data;
+
+	if (error == -EINPROGRESS)
+		return;
+	result->err = error;
+	complete(&result->completion);
+}
 
 static void
 hexdump(unsigned char *buf, unsigned int len)
 {
         while (len--)
 	{
-	        printk("%d", *buf++);
+	        printk("%02x", *buf++);
 	}
 
         printk("\n");
 }
 
-static int
-cryptoapi_demo(char operacao)
+static int cipher(int way,int numop)
 {
-	int i=0;
-        tfm = crypto_alloc_skcipher ("cbc(aes)", 0, 0);
-	if (IS_ERR(tfm)) 
-	{
-		printk("could nor allocate skcipher tfm\n");
-		return -1;
+	struct crypto_skcipher *tfm;
+	struct skcipher_request *req;
+	struct scatterlist sg;
+	char key[16], iv[16];
+	int i = 0;
+	int ret;
+	char *input = NULL;
+	struct tcrypt_result result;
+	
+//////////////////////////////////////////////////////////////////////
+
+	while(i != 16) {
+	
+		key[i] = key_getu[i];
+		iv[i] = iv_get[i];
+		i++;
+	}
+
+///////////////////////////////////////////////////////////////////////
+
+	tfm = crypto_alloc_skcipher("cbc(aes)", 0, 0);
+	if (IS_ERR(tfm)) {
+		ret = PTR_ERR(tfm);
+		return ret;
 	}
 
 	req = skcipher_request_alloc(tfm, GFP_KERNEL);
-    	if (!req) {
-        	printk("could not allocate tfm request\n");
-        	goto out;
-    	}
+	if (IS_ERR(req)) {
+		pr_err("ERROR: skcipher_request_alloc\n");
+		ret = PTR_ERR(req);
+		goto error_tfm;
+	}
 
-        ret = crypto_skcipher_setkey(tfm, key, strlen(key));
+	skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG, test_skcipher_cb, &result);
 
-        if (ret) {
-                printk(KERN_ERR PFX "error ret\n");
-                goto out;
-        }
 
-        input = kmalloc(DATA_SIZE, GFP_KERNEL);
+	ret = crypto_skcipher_setkey(tfm, key, DATA_SIZE);
+	if (ret != 0) {
+		pr_err("ERROR: crypto_skcipher_setkey\n");
+		goto error_req;
+	}
+
+	input = kmalloc(DATA_SIZE, GFP_KERNEL);
         if (!input) {
-                printk(KERN_ERR PFX "kmalloc(input) failed\n");
+                printk("kmalloc(input) failed\n");
                 goto out;
         }
 
-        output = kmalloc(DATA_SIZE, GFP_KERNEL);
-        if (!output) {
-                printk(KERN_ERR PFX "kmalloc(output) failed\n");
-                kfree(input);
-                goto out;
-        }
+	i=0;
 	while(i!=16)
 	{
-		input[i] = msg[i];
+		input[i] = msg[i+16*numop];
 		i++;
 	}
 	i=0;
-	sg_init_one(&sg[0], input, DATA_SIZE);
-	sg_init_one(&sg[1], output, DATA_SIZE);
-	skcipher_request_set_crypt(req, &sg[0], &sg[1], 16, iv);
-	if(operacao == 'c')
-	{
-		crypto_skcipher_encrypt(req);
-		printk(KERN_ERR PFX "input anterior: "); hexdump(input, 16);
-		printk(KERN_ERR PFX "output anterior: "); hexdump(output, 16);
+
+	sg_init_one(&sg, input, DATA_SIZE);
+
+	skcipher_request_set_crypt(req, &sg, &sg, DATA_SIZE, iv);
+	init_completion(&result.completion);
+
+	if (way == 0) {
+
+		printk("pre input: "); hexdump(input, DATA_SIZE);
+
+		ret = crypto_skcipher_encrypt(req);
+
+		printk("pos input: "); hexdump(input, DATA_SIZE);
 	}
-	if(operacao == 'd')
-	{
-		crypto_skcipher_decrypt(req);
-		printk("decrypted");
-		printk(KERN_ERR PFX "output decriptada: "); hexdump(output, 16);
+	else if(way == 1){
+
+		printk("pre output: "); hexdump(input, DATA_SIZE);
+
+		ret = crypto_skcipher_decrypt(req);
+
+		printk("pos output: "); hexdump(input, DATA_SIZE);
 	}
+	else {
+
+		goto out;
+	}
+
+	switch (ret) {
+	case 0:
+		break;
+	case -EINPROGRESS:
+	case -EBUSY:
+		ret = wait_for_completion_interruptible(&result.completion);
+		break;
+	}
+
 	while(i!=16)
 	{
-		msg[i] = output[i];
+		msg[i+16*numop] = input[i];
 		i++;
 	}
-        kfree(output);
-        kfree(input);
 
-out:
-        crypto_free_skcipher(tfm);
+error_req:
 	skcipher_request_free(req);
-	return 1;
+error_tfm:
+	crypto_free_skcipher(tfm);
+	return ret;
+out:
+	kfree(input);
+
+	return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+static int hash(void) {
+
+	struct crypto_shash *alg;
+	struct sdesc *sdesc;
+	char digest[60];
+	int ret, size;
+	alg = crypto_alloc_shash("sha1", 0, 0);
+	if (IS_ERR(alg)) {
+		return PTR_ERR(alg);
+	}
+
+	size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+	sdesc = kmalloc(size, GFP_KERNEL);
+	
+	sdesc->shash.tfm = alg;
+
+	if (IS_ERR(sdesc)) {
+		pr_info("can't alloc sdesc\n");
+		return PTR_ERR(sdesc);
+	}
+
+	ret = crypto_shash_digest(&sdesc->shash, msg, tamanhomsg, digest);
+	tamanhomsg = 20;
+	printk("SHA1: "); hexdump(digest, 20);
+	strcpy(msg,digest);
+	kfree(sdesc);
+	crypto_free_shash(alg);
+	return ret;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 static int dev_open(struct inode *inodep, struct file *filep){
+	if(!mutex_trylock(&crypto_mutex)){    /// Try to acquire the mutex (i.e., put the lock on/down)
+                                          /// returns 1 if successful and 0 if there is contention
+      		printk(KERN_ALERT "CryptoAPI: Device in use by another process");
+      		return -EBUSY;
+   	}
 	return 0;
 }
 
@@ -159,73 +248,70 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 	}
 }
 
-
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
 	int i = 0;
-	
-	if(!mutex_trylock(&crypto_mutex)){    /// Try to acquire the mutex (i.e., put the lock on/down)
-                                          /// returns 1 if successful and 0 if there is contention
-      		printk(KERN_ALERT "EBBChar: Device in use by another process");
-      		return -EBUSY;
-   	}
-	else {
-		sprintf(msg, "%s", buffer); 	
-		if(msg[0] == 'c')
+	int numop = 0;
+
+	sprintf(msg, "%s", buffer); 	
+	if(msg[0] == 'c')
+	{
+		while(msg[i+1]!='\0')
 		{
-			tamanhomsg = strlen(msg);
-			while(i!=17)
-			{
-				msg[i]=msg[i+1];
-				i++;
-			}
-			cryptoapi_demo('c');
-			cryptoapi_demo('d');
-		}        
-		if(msg[0] == 'd')
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+		numop = tamanhomsg/16;
+		i = 0;
+		while(i!=numop)
 		{
-			msg[0] = 11;
-			msg[1] = 155;
-			msg[2] = 21;
-			msg[3] = 218;
-			msg[4] = 75;
-			msg[5] = 68;
-			msg[6] = 160;
-			msg[7] = 245;
-			msg[8] = 21;
-			msg[9] = 29;
-			msg[10] = 207;
-			msg[11] = 196;
-			msg[12] = 192;
-			msg[13] = 31;
-			msg[14] = 53;
-			msg[15] = 213;
-			/*while(i!=17)
-			{
-				msg[i]=msg[i+1];
-				i++;
-			}*/
-			cryptoapi_demo('d');
-		}      
-		return 1;
-	}
-        return 0;
+			cipher(0,i);
+			i++;
+		}
+	}        
+	else if(msg[0] == 'd')
+	{
+		while(msg[i+1]!='\0')
+		{
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+		numop = tamanhomsg/16;
+		i = 0;
+		while(i!=numop)
+		{
+			cipher(1,i);
+			i++;
+		}
+	}   
+	else if(msg[0] == 'h')
+	{
+		while(msg[i+1]!='\0')
+		{
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+		hash();
+	} 
+	return 1;
 	
 }
 
 
 static int dev_release(struct inode *inodep, struct file *filep){
 	mutex_unlock(&crypto_mutex);
-
 	return 0;
 }
 
-/* ====== Module init/exit ====== */
+///////////////////////////////////////////////////////////////////////////////
 
-static int __init
-init_cryptoapi_demo(void)
+static int __init cryptotest_init(void)
 {
-
-	//Register major number
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
 	if (majorNumber<0)
 	{
@@ -233,41 +319,33 @@ init_cryptoapi_demo(void)
 	      return majorNumber;
 	}
 	 
-	// Register the device class
 	cryptoClass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(cryptoClass)) // Check for error and clean up if there is
+	if (IS_ERR(cryptoClass))
 	{              
 		unregister_chrdev(majorNumber, DEVICE_NAME);
 		printk(KERN_ALERT "Failed to register device class\n");
-		return PTR_ERR(cryptoClass);          // Correct way to return an error on a pointer
+		return PTR_ERR(cryptoClass);
 	}
 	 
-	// Register the device driver
 	cryptoDevice = device_create(cryptoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-	if (IS_ERR(cryptoDevice)) // Clean up if there is an error
+	if (IS_ERR(cryptoDevice))
 	{               
-		class_destroy(cryptoClass);           // Repeated code but the alternative is goto statements
+		class_destroy(cryptoClass);
 		unregister_chrdev(majorNumber, DEVICE_NAME);
 		printk(KERN_ALERT "Failed to create the device\n");
 		return PTR_ERR(cryptoDevice);
 	}
-	mutex_init(&crypto_mutex);
 
-        return 0;
+	return 0;
 }
 
-static void __exit
-exit_cryptoapi_demo(void)
+static void __exit cryptotest_exit(void)
 {
-	device_destroy(cryptoClass, MKDEV(majorNumber, 0));     // remove the device
-	class_unregister(cryptoClass);                          // unregister the device class
-	class_destroy(cryptoClass);                             // remove the device class
-	unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
-	mutex_destroy(&crypto_mutex);
+	device_destroy(cryptoClass, MKDEV(majorNumber, 0));
+	class_unregister(cryptoClass);
+	class_destroy(cryptoClass);
+	unregister_chrdev(majorNumber, DEVICE_NAME);  
 }
 
-module_init(init_cryptoapi_demo);
-module_exit(exit_cryptoapi_demo);
-
-
-
+module_init(cryptotest_init);
+module_exit(cryptotest_exit);
--- cryptoteste.c
+++ cryptoteste.c
@@ -5,78 +5,71 @@
 #include<string.h>
 #include<unistd.h>
  
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 256              
+static char receive[BUFFER_LENGTH];    
  
 int main(){
 	int ret, fd, i=0;
 	char stringToSend[BUFFER_LENGTH];
-	fd = open("/dev/crypto", O_RDWR);             // Open the device with read/write access
+	int tamanhostr;
+	fd = open("/dev/crypto", O_RDWR);             // Abre o device
 	if (fd < 0){
 		perror("Failed to open the device...");
 		return errno;
 	}
   
-	strcpy(stringToSend,"c0123456789abcdef");
-	ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+	strcpy(stringToSend,"c0123456789abcdefabcdef0123456789");//Teste criptografia
+	ret = write(fd, stringToSend, strlen(stringToSend)); //Escrever no device
 	if (ret < 0){
 		perror("Failed to write the message to the device.");
 		return errno;
 	}
  
-	ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+	ret = read(fd, receive, BUFFER_LENGTH);        // Receber os dados do device
 	if (ret < 0){
 		perror("Failed to read the message from the device.");
 		return errno;
 	}
+	tamanhostr = strlen(stringToSend)-1;
+	for(i=0;i<tamanhostr;i++)
+		printf("Mensagem criptada: [%d]\n", receive[i]);
 
-	close(fd);
-	for(i=0;i<16;i++)
-	printf("The enviada message is: [%d]\n", receive[i]);
-	strcpy(stringToSend, "d");
-	strcat(stringToSend,receive);
-/*	stringToSend[0] = 'd';
-	stringToSend[1] = 11;
-	stringToSend[2] = 155;
-	stringToSend[3] = 21;
-	stringToSend[4] = 218;
-	stringToSend[5] = 75;
-	stringToSend[6] = 68;
-	stringToSend[7] = 160;
-	stringToSend[8] = 245;
-	stringToSend[9] = 21;
-	stringToSend[10] = 29;
-	stringToSend[11] = 207;
-	stringToSend[12] = 196;
-	stringToSend[13] = 192;
-	stringToSend[14] = 31;
-	stringToSend[15] = 53;
-	stringToSend[16] = 213;
-	stringToSend[17] = '\0';*/
-
-
-
-	/*stringToSend[0] = 'd';
+	stringToSend[0] = 'd';//descriptografar o resultado da string criptografada acima
   	for(i = 0; receive[i]; i++)
 		stringToSend[i+1] = receive[i];
-	stringToSend[i+1] = '\0';*/
-	//printf("%s\n", stringToSend);
-/*	sleep(2);	
+	stringToSend[i+1] = '\0';
 
-	ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+
+	ret = write(fd, stringToSend, strlen(stringToSend)); // Escrever no device
 	if (ret < 0){
 		perror("Failed to write the message to the device.");
 		return errno;
 	}
  
-	ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+	ret = read(fd, receive, BUFFER_LENGTH);        // Receber os dados do device
 	if (ret < 0){
 		perror("Failed to read the message from the device.");
 		return errno;
 	}
-	for(i=0;i<16;i++)
-	printf("The recebida message is: [%d]\n", receive[i]);
-	close(fd);*/
+	tamanhostr = strlen(stringToSend)-1;
+	for(i=0;i<tamanhostr;i++)
+	printf("Mensagem decriptada: [%d]\n", receive[i]);
+
+	strcpy(stringToSend,"hteste");//Teste do Hashe
+	ret = write(fd, stringToSend, strlen(stringToSend)); // Escrever no device
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+ 
+	ret = read(fd, receive, BUFFER_LENGTH);        // Receber os dados do device
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+	printf("Hash:\n");
+	for(i=0;i<20;i++)
+		printf("[%hhx]", receive[i]);
 	close(fd);
    return 0;
 }
