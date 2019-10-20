fcd5d9b789e7a8779404ec863a2e166718729d6b
Matheus Valbert <matheusvalbert@gmail.com>
2019-10-20 01:46:38 -0300

    Add files via upload

--- crypto.c
+++ crypto.c
@@ -9,6 +9,7 @@
 #include <crypto/sha.h>
 #include <crypto/internal/skcipher.h>
 #include <linux/jiffies.h>
+#include <linux/mutex.h> 
 
 MODULE_AUTHOR("Projeto1 SO_B");
 MODULE_DESCRIPTION("crypto api");
@@ -19,11 +20,7 @@ MODULE_LICENSE("GPL");
 #define DATA_SIZE       16
 
 ///////////////////////////////////////////////////////////////////////////
-
-static char *key_get = "";
-static char *iv_get = "";
-
-
+static DEFINE_MUTEX(crypto_mutex);
 static int    majorNumber;              
 static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
@@ -36,10 +33,13 @@ static struct device* cryptoDevice = NULL;
 static char   msg[256] = {0};
 static int tamanhomsg = 0;
 
-module_param(key_get, charp, 0000);
-MODULE_PARM_DESC(key_get, "A string");
+static char *key_getu = "";
+static char *iv_get = "";
+
+module_param(key_getu, charp, 0000);
+MODULE_PARM_DESC(key_getu, "00000000000000000000");
 module_param(iv_get, charp, 0000);
-MODULE_PARM_DESC(iv_get, "A string");
+MODULE_PARM_DESC(iv_get, "00000000000000000000");
 
 static struct file_operations fops =
 {
@@ -49,12 +49,64 @@ static struct file_operations fops =
    .release = dev_release,
 };
 
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
+};
+
 ///////////////////////////////////////////////////////////////////////////
 
 struct tcrypt_result {
 	struct completion completion;
 	int err;
 };
+
+char convert(char op) {
+
+	switch(op) {
+
+	case '0':
+		return 0x0;
+	case '1':
+		return 0x1;
+	case '2':
+		return 0x2;
+	case '3':
+		return 0x3;
+	case '4':
+		return 0x4;
+	case '5':
+		return 0x5;
+	case '6':
+		return 0x6;
+	case '7':
+		return 0x7;
+	case '8':
+		return 0x8;
+	case '9':
+		return 0x9;
+	case 'A':
+	case 'a':
+		return 0xa;
+	case 'B':
+	case 'b':
+		return 0xb;
+	case 'C':
+	case 'c':
+		return 0xc;
+	case 'D':
+	case 'd':
+		return 0xd;
+	case 'E':
+	case 'e':
+		return 0xe;
+	case 'F':
+	case 'f':
+		return 0xf;
+	}
+
+	return 0;
+}
 
 static void test_skcipher_cb(struct crypto_async_request *req, int error)
 {
@@ -71,34 +123,40 @@ hexdump(unsigned char *buf, unsigned int len)
 {
         while (len--)
 	{
-	        printk("%d", *buf++);
+	        printk("%02x", *buf++);
 	}
 
         printk("\n");
 }
 
-static int cipher(int way)
+static int cipher(int way,int numop,char ms[])
 {
 	struct crypto_skcipher *tfm;
 	struct skcipher_request *req;
 	struct scatterlist sg;
-	char key[16], iv[16];
+	char key[20], iv[20];
 	int i = 0;
 	int ret;
 	char *input = NULL;
 	struct tcrypt_result result;
-	
 //////////////////////////////////////////////////////////////////////
-
 	while(i != 16) {
 	
-		key[i] = key_get[i];
+		key[i] = key_getu[i];
+		if(key[i]=='\0')
+		{
+			key[i]='0';
+		}
 		iv[i] = iv_get[i];
+		if(iv[i]=='\0')
+		{
+			iv[i]='0';
+		}
 		i++;
 	}
-
-///////////////////////////////////////////////////////////////////////
-
+	key[16]='\0';
+	iv[16]='\0';
+//////////////////////////////////////////////////////////////////////
 	tfm = crypto_alloc_skcipher("cbc(aes)", 0, 0);
 	if (IS_ERR(tfm)) {
 		ret = PTR_ERR(tfm);
@@ -130,7 +188,7 @@ static int cipher(int way)
 	i=0;
 	while(i!=16)
 	{
-		input[i] = msg[i];
+		input[i] = ms[i+16*numop];
 		i++;
 	}
 	i=0;
@@ -172,7 +230,7 @@ static int cipher(int way)
 
 	while(i!=16)
 	{
-		msg[i] = input[i];
+		msg[i+16*numop] = input[i];
 		i++;
 	}
 
@@ -183,19 +241,61 @@ error_tfm:
 	return ret;
 out:
 	kfree(input);
-
+	key[0]='\0';
+	iv[0]='\0';
 	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
+static int hash(void) {
+
+	struct crypto_shash *alg;
+	struct sdesc *sdesc; 
+	char digest[60];
+	int ret, size;
+	alg = crypto_alloc_shash("sha1", 0, 0);
+	if (IS_ERR(alg)) {
+		return PTR_ERR(alg);
+	}
+
+	size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+	sdesc = kmalloc(size, GFP_KERNEL);
+	
+	sdesc->shash.tfm = alg;
+
+	if (IS_ERR(sdesc)) {
+		pr_info("can't alloc sdesc\n");
+		return PTR_ERR(sdesc);
+	}
+
+	ret = crypto_shash_digest(&sdesc->shash, msg, tamanhomsg, digest);
+	tamanhomsg = 20;
+	printk("SHA1: "); hexdump(digest, 20);
+	strcpy(msg,digest);
+	kfree(sdesc);
+	crypto_free_shash(alg);
+	return ret;
+}
+
+//////////////////////////////////////////////////////////////////////////////// f04d5c594c0de21b11a194844305ff3e
+
 static int dev_open(struct inode *inodep, struct file *filep){
+	if(!mutex_trylock(&crypto_mutex)){    /// Try to acquire the mutex (i.e., put the lock on/down)
+                                          /// returns 1 if successful and 0 if there is contention
+      		printk(KERN_ALERT "CryptoAPI: Device in use by another process");
+      		return -EBUSY;
+   	}
 	return 0;
 }
 
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
 	int error_count = 0;
+	//int numop;
 // copy_to_user has the format ( * to, *from, size) and returns 0 on success
+	//printk("retorno");hexdump(msg, tamanhomsg);
+	//numop = tamanhomsg/16;
+	//tamanhomsg = (numop+1)*16 - tamanhomsg%16;
 	error_count = copy_to_user(buffer,msg, tamanhomsg);
 	if (error_count==0){            
 		return 1;
@@ -207,43 +307,134 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 }
 
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-	int i = 0;
-	
-	if(1 == 0){    /// Try to acquire the mutex (i.e., put the lock on/down)
-                                          /// returns 1 if successful and 0 if there is contention
-      		printk(KERN_ALERT "CryptoAPI: Device in use by another process");
-      		return -EBUSY;
-   	}
-	else {
-		sprintf(msg, "%s", buffer); 	
-		if(msg[0] == 'c')
+	int i = 0, j=0;
+	int numop = 0;
+	char *input = NULL;
+
+	sprintf(msg, "%s", buffer); 	
+	if(msg[0] == 'c')
+	{
+		//printk("mensagem %s", msg);
+
+		while(msg[i+1]!='\0')
 		{
-			tamanhomsg = strlen(msg);
-			while(i!=17)
-			{
-				msg[i]=msg[i+1];
-				i++;
-			}
-			cipher(0);
-		}        
-		else if(msg[0] == 'd')
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+
+		input = kmalloc(128, GFP_KERNEL);
+		if (!input) {
+		        printk("kmalloc(input) failed\n");
+			goto outc;
+		}
+
+		i=0;
+		while(msg[i] != '\0') {
+			
+			msg[i] = convert(msg[i]);
+			i++;
+		}
+
+		i=0;
+		while(i != tamanhomsg/2) {
+
+			input[i] = (msg[j]<<4) + msg[j+1];		
+			
+			i++;
+			j+=2;
+		}
+		
+		while(i % 16 != 0)
 		{
-			while(i!=17)
-			{
-				msg[i]=msg[i+1];
-				i++;
-			}
-			cipher(1);
-		}      
-		return 1;
-	}
-        return 0;
+			
+			input[i] = 0x00;	
+			i++;
+		}
+		input[i] = '\0';
+		tamanhomsg = i;
+		//////////////////AQUIIIIII///////////////////////
+		//printk("teste %i", tamanhomsg); hexdump(input, 20);
+		i = 0;
+		numop = tamanhomsg/16;
+		while(i!=numop)
+		{
+			//printk("cipher no %i %i", numop,tamanhomsg);
+			cipher(0,i,input);
+			i++;
+		}		
+
+outc:		kfree(input);
+
+	}        
+	else if(msg[0] == 'd')
+	{
+		while(msg[i+1]!='\0')
+		{
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+
+
+		input = kmalloc(128, GFP_KERNEL);
+		if (!input) {
+		        printk("kmalloc(input) failed\n");
+			goto outd;
+		}
+
+		i=0;
+		while(msg[i] != '\0') {
+			
+			msg[i] = convert(msg[i]);
+			i++;
+		}
+
+		//printk("Ak"); hexdump(msg, 32);
+		
+		i=0;
+		while(i != tamanhomsg/2) {
+
+			input[i] = (msg[j]<<4) + msg[j+1];		
+			
+			i++;
+			j+=2;
+		}
+		tamanhomsg = i;
+
+		//printk("AQUI"); hexdump(input, 16);
+		//////////////////AQUIIIIII///////////////////////
+		//printk("teste %i", tamanhomsg); hexdump(input, 20);
+		i = 0;
+		numop = tamanhomsg/16;
+		while(i!=numop)
+		{
+			//printk("cipher no %i %i", numop,tamanhomsg);
+			cipher(1,i,input);
+			i++;
+		}
+outd:		kfree(input);
+	}   
+	else if(msg[0] == 'h')
+	{
+		while(msg[i+1]!='\0')
+		{
+			msg[i]=msg[i+1];
+			i++;
+		}
+		msg[i] = '\0';
+		tamanhomsg = strlen(msg);
+		hash();
+	} 
+	return 1;
 	
 }
 
 
 static int dev_release(struct inode *inodep, struct file *filep){
-
+	mutex_unlock(&crypto_mutex);
 	return 0;
 }
 
@@ -251,6 +442,7 @@ static int dev_release(struct inode *inodep, struct file *filep){
 
 static int __init cryptotest_init(void)
 {
+	
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
 	if (majorNumber<0)
 	{
--- cryptoteste.c
+++ cryptoteste.c
@@ -4,77 +4,167 @@
 #include<fcntl.h>
 #include<string.h>
 #include<unistd.h>
+#include <stdio_ext.h>
  
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
- 
-int main(){
-	int ret, fd, i=0;
-	char stringToSend[BUFFER_LENGTH];
-	fd = open("/dev/crypto", O_RDWR);             // Open the device with read/write access
+#define BUFFER_LENGTH 256              
+static char receive[BUFFER_LENGTH];
+    
+int hex_to_int(char c){
+        int first = c / 16 - 3;
+        int second = c % 16;
+        int result = first*10 + second;
+        if(result > 9) result--;
+        return result;
+}
+
+int hex_to_ascii(char c, char d){
+        int high = hex_to_int(c) * 16;
+        int low = hex_to_int(d);
+        return high+low;
+}
+
+void hexdump(unsigned char *buf, unsigned int len)
+{
+        while (len--)
+	{
+	        printf("%02x", *buf++);
+	}
+
+	printf("\n");
+}
+
+int main()
+{
+	int ret, fd, i=0, flag = 0;
+	char op;
+	char stringToSend[BUFFER_LENGTH], stringReceive[BUFFER_LENGTH];
+	char buf;
+	int tamanhostr;
+	fd = open("/dev/crypto", O_RDWR);             // Abre o device
 	if (fd < 0){
 		perror("Failed to open the device...");
 		return errno;
 	}
-  
-	strcpy(stringToSend,"c0123456789abcdef");
-	ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-	if (ret < 0){
-		perror("Failed to write the message to the device.");
-		return errno;
-	}
- 
-	ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-	if (ret < 0){
-		perror("Failed to read the message from the device.");
-		return errno;
-	}
-	for(i=0;i<16;i++)
-	printf("The enviada message is: [%d]\n", receive[i]);
+	
+	do
+	{
+		printf("Choose option:\n");
+		printf("	1 - String\n");
+		printf("	2 - Hexa\n");
+		printf("	3 - Sair\n");
+		scanf("%c",&op);
+		
+		switch(op)
+		{
+		case '1':
+			printf("String:");
+			__fpurge(stdin);
+	    		fgets(stringReceive, sizeof(stringReceive), stdin);
 
-	stringToSend[0] = 'd';
-  	for(i = 0; receive[i]; i++)
-		stringToSend[i+1] = receive[i];
-	stringToSend[i+1] = '\0';
-	//printf("%s\n", stringToSend);
+	    		tamanhostr = strlen(stringReceive);
+	    		if(stringReceive[tamanhostr-1]=='\n')
+				stringReceive[--tamanhostr] = '\0';
+			stringToSend[0]=stringReceive[0];
+			if(stringToSend[0]=='c')
+			{
+		    		for(i = 0; i<tamanhostr-1; i++)
+				{
+					sprintf(stringToSend+i*2+1, "%02X", stringReceive[i+1]);
+		    		}
+		    		printf("String to send(hexa): %s\n", stringToSend);
+			}
+			ret = write(fd, stringToSend, strlen(stringToSend)); //Escrever no device
+			if (ret < 0){
+					perror("Failed to write the message to the device.");
+					return errno;
+			}
+				 
+			ret = read(fd, receive, BUFFER_LENGTH);        // Receber os dados do device
+			if (ret < 0){
+					perror("Failed to read the message from the device.");
+					return errno;
+			}
+			tamanhostr = strlen(receive);
+			switch(stringToSend[0])
+			{
+				case 'c':
+					printf("\nMensagem criptografada: ");
+					hexdump(receive,tamanhostr);
+					break;
+				case 'd':
+					printf("\nMensagem descriptografada: ");
+					for(i=0;i<tamanhostr;i++)
+						hexdump(receive,tamanhostr);
+					break;
+				case 'h':
+					printf("Resultado do Hash: ");
+					hexdump(receive,20);
+					break;
+				default:
+					printf("Operacao invalida.\n");
+					break;
+			}
+			break;	
+		case '2':
+			printf("Hexa:");
+			__fpurge(stdin);
+			fgets(stringToSend, sizeof(stringToSend), stdin);
+			tamanhostr = strlen(stringToSend);
+			printf("String to send(hexa): %s", stringToSend);
+			for(i = 1; i<tamanhostr; i++)
+			{
+			        if(i % 2 != 1)
+				{
+		                	printf("%c", hex_to_ascii(buf, stringToSend[i]));
+		        	}
+				else
+				{
+		                	buf = stringToSend[i];
+		        	}
+			}
+			stringToSend[i]='\0';
 
+			ret = write(fd, stringToSend, strlen(stringToSend)); //Escrever no device
+			if (ret < 0){
+					perror("Failed to write the message to the device.");
+					return errno;
+			}
+				 
+			ret = read(fd, receive, BUFFER_LENGTH);        // Receber os dados do device
+			if (ret < 0){
+					perror("Failed to read the message from the device.");
+					return errno;
+			}
+			tamanhostr = strlen(receive);			
 
-	ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-	if (ret < 0){
-		perror("Failed to write the message to the device.");
-		return errno;
-	}
- 
-	ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-	if (ret < 0){
-		perror("Failed to read the message from the device.");
-		return errno;
-	}
-	for(i=0;i<16;i++)
-	printf("The recebida message is: [%d]\n", receive[i]);
+			switch(stringToSend[0])
+			{
+				case 'c':
+					printf("\nMensagem criptografada: ");
+					hexdump(receive,tamanhostr);
+					break;
+				case 'd':
+					printf("\nMensagem descriptografada: ");
+					hexdump(receive,tamanhostr);
+					break;
+				case 'h':
+					printf("Resultado do Hash: ");
+					hexdump(receive,20);
+					break;
+				default:
+					printf("Operacao invalida.\n");
+					break;
+			}
+			break;	
+		case '3':
+			flag = 1;
+			break;
+		default:
+			printf("Opcao invalida.\n");
+			break;
+		}
+
+	}while(flag == 0);
+	close(fd);
    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
